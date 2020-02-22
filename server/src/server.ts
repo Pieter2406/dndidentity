6df9ed07c6e1385dc2cc85e7309f769bdc9c0b60
PIeter <pieter.verlinden@outlook.com>
2020-02-22 13:12:38 +0100

    Change server message

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,9 +10,7 @@ app.listen(
   process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 2406,
   () => {
     console.log(
-      `server started at ${process.env.IP ||
-        process.env.OPENSHIFT_NODEJS_IP ||
-        'localhost'}:${process.env.PORT ||
+      `server started at Port:${process.env.PORT ||
         process.env.OPENSHIFT_NODEJS_PORT ||
         2406}`
     );
