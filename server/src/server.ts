b01452058aaf47518a34e195b7e55feee6bba4b1
PIeter <pieter.verlinden@outlook.com>
2020-02-22 13:02:15 +0100

    Test port on image

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,9 +6,15 @@ app.get('/', (req, res) => {
   res.send('hello dndidentiy guys');
 });
 
-app.listen(process.env.PORT || 2406, () => {
-  console.log(
-    `server started at ${process.env.OPENSHIFT_NODEJS_IP ||
-      'localhost'}:${process.env.OPENSHIFT_NODEJS_PORT || 2406}`
-  );
-});
+app.listen(
+  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 2406,
+  () => {
+    console.log(
+      `server started at ${process.env.IP ||
+        process.env.OPENSHIFT_NODEJS_IP ||
+        'localhost'}:${process.env.PORT ||
+        process.env.OPENSHIFT_NODEJS_PORT ||
+        2406}`
+    );
+  }
+);
